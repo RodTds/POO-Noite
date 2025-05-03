31a564c6a90792b932f494550df0817eab1d7d09
RodTds <rodrigo.silva505@fatec.sp.gov.br>
2025-05-03 15:50:52 -0300

    atividade heranca e polimorfismo

--- AtividadePolimorfismo/GerenciadorBonificacao.cs
+++ AtividadePolimorfismo/GerenciadorBonificacao.cs
@@ -7,31 +7,61 @@ namespace AtividadePolimorfismo
 {
     public class GerenciadorBonificacao
     {
-        private double totalBonificacao;
-        public double TotalBonificacao
+        private double totalFuncionario;
+        private double totalDiretor;
+        private double totalgerente;
+        private double totalSecretario;
+
+        public double TotalFuncionario
         {
-            get { return totalBonificacao; }
-            set { totalBonificacao = value; }
+            get { return totalFuncionario; }
+            set { totalFuncionario = value; }
+        }
+
+        public double TotalDiretor
+        {
+            get { return totalDiretor; }
+            set { totalDiretor = value; }
         }  
-         // se aqui eu colocar o parametro no tipo funcionario eu consigo na instancia jogar qualquer objeto herdado
-        public void TotalizadorBonificacao(Funcionario funcionario){
-            this.TotalBonificacao += funcionario.CalcularBonificacao();
-        
-        }
-        public void TotalizadorBonificacao(Diretor diretor){
-            this.TotalBonificacao += diretor.CalcularBonificacao();
-            
-        }
-         public void TotalizadorBonificacao(Gerente gerente){
-            this.TotalBonificacao += gerente.CalcularBonificacao();
-           
-        }
-        public void TotalizadorBonificacao(Secretario secretario){
-            this.TotalBonificacao += secretario.CalcularBonificacao();
-           
-        }
-       
         
      
+        public double TotalGerente
+        {
+            get { return totalgerente; }
+            set { totalgerente = value; }
+        }
+
+        public double TotalSecretario
+        {
+            get { return totalSecretario; }
+            set { totalSecretario = value;}
+        }
+
+
+
+        // se aqui eu colocar o parametro no tipo funcionario eu consigo na instancia jogar qualquer objeto herdado
+        public void TotalizadorBonificacao(Funcionario funcionario)
+        {
+            this.TotalFuncionario += funcionario.CalcularBonificacao();
+
+        }
+        public void TotalizadorBonificacao(Diretor diretor)
+        {
+            this.TotalDiretor += diretor.CalcularBonificacao();
+
+        }
+        public void TotalizadorBonificacao(Gerente gerente)
+        {
+            this.TotalGerente += gerente.CalcularBonificacao();
+
+        }
+        public void TotalizadorBonificacao(Secretario secretario)
+        {
+            this.TotalSecretario += secretario.CalcularBonificacao();
+
+        }
+
+
+
     }
 }
--- AtividadePolimorfismo/Program.cs
+++ AtividadePolimorfismo/Program.cs
@@ -17,9 +17,87 @@ GerenciadorBonificacao gerenciar = new GerenciadorBonificacao();
 gerenciar.TotalizadorBonificacao(diretor);
 gerenciar.TotalizadorBonificacao(diretor2);
 
+gerenciar.TotalizadorBonificacao(secretario);
+gerenciar.TotalizadorBonificacao(secretario2);
+
 gerenciar.TotalizadorBonificacao(gerente2);
 gerenciar.TotalizadorBonificacao(gerente);
 
 // Exiber Resultados
-System.Console.WriteLine("Total Bonificação dos diretores: R$ " + gerenciar.TotalBonificacao);
+System.Console.WriteLine($"Total Bonificação dos Funcionarios: { gerenciar.TotalFuncionario:c}" );
+System.Console.WriteLine($"Total Bonificação dos Diretores: { gerenciar.TotalDiretor:c}" );
+System.Console.WriteLine($"Total Bonificação dos Gerentes: { gerenciar.TotalGerente:c}" );
+System.Console.WriteLine($"Total Bonificação dos Secretários: { gerenciar.TotalSecretario:c}" );
+
+
+/* 
+  Existe uma forma usando Dictionary mas a
+  Forma mais simples seria usando na classe GerenciadorBonificacao
+  
+ public class GerenciadorBonificacao
+{
+    public double TotalBonificacaoSecretario { get; private set; }
+    public double TotalBonificacaoGerente { get; private set; }
+    public double TotalBonificacaoDiretor { get; private set; }
+
+    public void TotalizadorBonificacao(Funcionario funcionario)
+    {
+        if (funcionario is Secretario)
+        {
+            TotalBonificacaoSecretario += funcionario.CalcularBonificacao();
+        }
+        else if (funcionario is Gerente)
+        {
+            TotalBonificacaoGerente += funcionario.CalcularBonificacao();
+        }
+        else if (funcionario is Diretor)
+        {
+            TotalBonificacaoDiretor += funcionario.CalcularBonificacao();
+        }
+    }
+
+    public double TotalBonificacaoGeral => TotalBonificacaoSecretario + TotalBonificacaoGerente + TotalBonificacaoDiretor;
+}
+
+PROGRAM.CS
+
+// Secretarios
+Secretario secretario = new Secretario(1,"Joao",3000);
+Secretario secretario2 = new Secretario(6,"Joana",3000);
+Secretario secretario3 = new Secretario(7,"Marcela",3000);
+
+// Gerentes
+Gerente gerente = new Gerente(2,"Carlos",3000);
+Gerente gerente2 = new Gerente(8,"Linda",3000);
+Gerente gerente3 = new Gerente(9,"Marcia",3000);
+
+// Diretores
+Diretor diretor = new Diretor(3,"Marcos",3000);
+Diretor diretor2 = new Diretor(4,"Joao",3000);
+Diretor diretor3 = new Diretor(5,"Carlos",3000);
+
+// Gerenciador
+GerenciadorBonificacao gerenciar = new GerenciadorBonificacao();
+
+// Adiciona bonificações
+gerenciar.TotalizadorBonificacao(secretario);
+gerenciar.TotalizadorBonificacao(secretario2);
+gerenciar.TotalizadorBonificacao(secretario3);
+
+gerenciar.TotalizadorBonificacao(gerente);
+gerenciar.TotalizadorBonificacao(gerente2);
+gerenciar.TotalizadorBonificacao(gerente3);
+
+gerenciar.TotalizadorBonificacao(diretor);
+gerenciar.TotalizadorBonificacao(diretor2);
+gerenciar.TotalizadorBonificacao(diretor3);
+
+// Exibe Resultados
+Console.WriteLine("Bonificação total de Secretários: R$ " + gerenciar.TotalBonificacaoSecretario);
+Console.WriteLine("Bonificação total de Gerentes: R$ " + gerenciar.TotalBonificacaoGerente);
+Console.WriteLine("Bonificação total de Diretores: R$ " + gerenciar.TotalBonificacaoDiretor);
+Console.WriteLine("Bonificação Geral: R$ " + gerenciar.TotalBonificacaoGeral);
+
+
+*/
 
